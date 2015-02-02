0c2e1b301588d7ab96d2a24aaf778998e95afa86
alexamies <alexamies@gmail.com>
2015-02-02 07:28:50 -0800

    Tidied up some top level pages.

--- web/culture.php
+++ web/culture.php
@@ -1,295 +1,117 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
-    <title>Chinese Notes - Chinese Culture 汉语笔记 - 中国文化</title>
-    <link rel="shortcut icon" href="/favicon.ico"/>
-    <link rel="stylesheet" type="text/css" href="styles.css"/>
-    <link media="only screen and (max-device-width: 480px)" href="styles-small-device.css" type= "text/css" 
-          rel="stylesheet"/>
-    <meta name="keywords"
-          content="Chinese Notes - Chinese Culture Bilingual 汉语笔记 - 中国文化 语言"/>
-    <meta name="description"
-          content="Chinese Notes - Bilingual Language Reference Materials 汉语笔记 - 语言资料资料"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Chinese language notes">
+    <link rel="shortcut icon" href="images/ren.png" type="image/jpeg" />
+    <title>Chinese language notes</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="chinesenotes.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
   </head>
   <body>
-    <div class="menubar">
-      <a class='button' href='index.html'>Home<span class='cn'> - 首页</span></a>
-      <a class='button' href='tools.php'>Tools<span class='cn'> - 工具</span></a>
-      <a class='selected' href='culture.php'>Culture<span class='cn'> - 文化</span></a>
-      <a class='button' href='reference.php'>Reference<span class='cn'> - 参考</span></a>
-      <a class='button' href='classics.php'>Classics<span class='cn'> - 古文</span></a>
+    <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-59206430-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <div class="starter-template">
+      <div class="row">
+        <div class="span2"><img id="logo" src="images/ren.png" alt="Logo" class="pull-left"/></div>
+        <div class="span7"><h1>Chinese Notes</h1></div>
+      </div>
     </div>
-    <div class="breadcrumbs">
-      <a href="index.html">Chinese Notes 中文笔记</a> &gt; 
-      Culture 文化
-    </div>      
-    <h1>Chinese Culture 中国文化</h1>
-    <table class="portlet">
-    	<tbody>
-  			<tr>
-  				<td width="75%">
-  					<h4><a href='decorative_designs.php'>中国文物的纹饰 Decorative Designs in Chinese Art</a></h4>
-  					<p>
-  					  本文章描述的是淘气、青铜器以及其他文物的纹饰。纹饰与美术的区别是纹饰用可重复图案而带纹饰的文物有本身的作用。纹饰可以包涵中国文化的故事，有的无法仅用言语表达的。
-  					</p>
-  					<p>
-  					  This article describes decorative art on pottery, bronze ware, and many other art works and artifacts. I distinguish decorative art from fine art in that decorative art is most often uses repeated designs and is applied to artifacts that have functions in themselves. Decorative art has a fascinating story to tell about Chinese culture that is not as easily told using words alone.
-  					</p>
-  				</td>
-  				<td width="25%">
-  					<img src='images/firedesign_small.png' alt='Fire Design 火纹' title='Fire Design 火纹'/></a>
-  				</td>
-  			</tr>
-  			<tr>
-  				<td colspan="2">
-  					<h4><a href='ethnic_groups.php'>中华民族 Chinese Ethnic Groups</a></h4>
-  					<p>
-  					  China has 56 ethnic groups.  The English and Chinese names for each and the
-  					  regions that they inhabit are listed here.
-  					</p>
-  				</td>
-  			</tr>
-    	</tbody>
-    </table>
-    <br/>
-    <table class="portlet">
-    	<tbody>
-    		<tr>
-    	      <td colspan="2">
-    		    <h3 class="portlet">
-    		     书法入门 Introduction to Calligraphy
-    		    </h3>
-    		  </td>
-    		</tr>
-    		<tr>
-    		  <td colspan="2">
-    			<p>
-    			  <a href='/calligraphy.php'>书法入门 Introduction to Calligraphy</a> &mdash;
-    			  介绍文具、执笔与运笔、笔法、基本笔画（颜体）和临摹     <br/>
-    			  Introduces materials, holding and moving the brush, drawing strokes, fundamental strokes (Yanti), and
-    			  copying.
-    			</p>
-    		  </td>
-    		</tr>
-    	</tbody>
-    </table>
-    <br/>
-    <table class="portlet">
-    	<tbody>
-    		<tr>
-    	      <td colspan="2">
-    		    <h3 class="portlet">
-    		     文章 Articles
-    		    </h3>
-    		  </td>
-    		</tr>
-    		<tr>
-    		  <td colspan="2">
-    			<p>
-    			  <a href='/articles.php'>文章 Articles</a> &mdash;
-    			  从大陆媒体中英双言包括口音和生词定义。<br/>
-    			  Chinese and English versions of the articles from the Mainland media with audio and 
-      links to all the vocabulary used.
-    			</p>
-    		  </td>
-    		</tr>
-    	</tbody>
-    </table>
-    <br/>
-    <table class="portlet">
-    	<tbody>
-    		<tr>
-    	      <td colspan="2">
-    		    <h3 class="portlet">
-    		     感恩词 Words of Thanks
-    		    </h3>
-    		  </td>
-    		</tr>
-    		<tr>
-    		  <td colspan="2">
-    			<p>
-    			  <a href='/culture_ganenci.php'>感恩词 Words of Thanks</a>
-    			</p>
-    		  </td>
-    		</tr>
-    	</tbody>
-    </table>
-    <br/>
-    <table class="portlet">
-    	<tbody>
-    		<tr>
-    	      <td colspan="2">
-    		    <h3 class="portlet">
-    		      名胜古迹 Historic Sites and Scenic Places
-    		    </h3>
-    		  </td>
-    		</tr>
-    		<tr>
-    		  <td colspan="2">
-    			<h4>
-    			  <a href='/history_lidaidiwangsi.php'>历代帝王庙 Temple of Ancient Monarchs</a>
-    			</h4>
-    			<p>
-    			  历代帝王庙始建于明朝嘉靖九年（1530） ...<br/>
-    			  The Temple of Ancient Monarchs was first built in the ninth year of the Ming Emperor 
-    			  Jia Jing (1530) ...
-    			</p>
-    		  </td>
-    		</tr>
-            <tr>
-              <td colspan="2">
-                <h4>
-                  <a href='/buddhism_chinese.html'>中国佛教 Chinese Buddhism</a>
-                </h4>
-                <p>
-                  Lists, maps, and background for Chinese Buddhist sites
-                </p>
-              </td>
-            </tr>
-    		<tr>
-    		  <td>
-    			<h4>
-    			  <a href='/history_hongluosi.php'>红螺寺 Hongluo Temple</a>
-    			</h4>
-    			<p>
-    			  此寺为东晋咸康四年（338年）高僧佛图澄所创建<br/>
-    			  This temple was built by the senior monk Fo Tucheng in the fourth year of the reign of Emperor Xian Kang 
-    			  in the Eastern Jin period (338 AD).
-    			</p>
-    		  </td>
-    		  <td  rowspan="2"style="text-align: right;">
-                <a href='illustrations_use.php?mediumResolution=huangluo3_med.jpg'><img class='use' 
-                   src='images/huangluo3_med.jpg' alt='红螺寺 Hongluo Temple' 
-                   title='红螺寺Hongluo Temple'/></a>
-    		  </td>
-    		</tr>
-    		<tr>
-    		  <td>
-    			<h4>
-    			  <a href='/history_gongwangfu.php'>恭王府 Prince Gong's Palace</a>
-    			</h4>
-    			<p>
-    			恭王府是北京现存清代王府中布署最精美，保存最完善的一处<br/>
-    			Prince Gong's Palace is one of Beijing's still existing, most refined, best preserved, and complete Qing 
-    			Dynasty declared bureau. 
-    			</p>
-    		  </td>
-    		</tr>
-    	</tbody>
-    </table>
-    <br/>
-    <table class="portlet">
-    	<tbody>
-    		<tr>
-    			<th class="portlet" colspan="2">
-    				<h2 class="portlet">
-    					<br />
-    				</h2>
-    			</th>
-    		</tr>
-    		<tr>
-    			<td width="75%">
-    				<h3 class="portlet">
-    					食品和烹调
-    					<br />
-    					Food and Cooking
-    				</h3>
-    			</td>
-    			<td style="text-align: right;">
-    				<a href="food.html">
-    					<img alt="Dumplings" title="Dumplings"
-    						src="images/dumpling_2.jpg"
-    						style="width: 194px; height: 144px;" border="0" />
-    				</a>
-    				<br />
-    				<br />
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<p>
-    					介绍关于食品和烹调。
-    					<a href="food.html">食品</a>
-    					：
-    					<a href="eating_drinking.html">吃喝</a>
-    					，
-    					<a href="fruit.html">水果</a>
-    					，
-    					<a href="vegetables.html">蔬菜</a>
-    					，
-    					<a href="soy.html">豆类</a>
-    					，
-    					<a href="meat_seafood.html">肉，蛋，海鲜</a>
-    					，
-    					<a href="cooking.html">烹调</a>
-    					，
-    					<a href="drinks.html">饮料</a>
-    					,
-    					<a href="condiments.html">作料</a>
-    					，
-    					<a href="grains.html">粮食</a>
-    					，
-    					<a href="mushrooms.html">菌类</a>
-    					，
-    					<a href="sweets.html">甜食</a>
-    					，
-    					<a href="chinese_food.html">中餐</a>
-    					，
-    					<a href="western_food.html">西餐</a>
-    					,和
-    					<a href="nutrition.html">饮 食健康</a>
-    					。 文章：
-    					<a href="bones_article.html">水果竟比牛奶更壮骨？</a>
-    				</p>
-    				<p>
-    					Introduces Chinese language relating to food and
-    					cooking.&nbsp;
-    					<a href="food.html">Food</a>
-    					:&nbsp;
-    					<a href="eating_drinking.html">
-    						Eating and drinking
-    					</a>
-    					,
-    					<a href="fruit.html">fruit</a>
-    					,
-    					<a href="vegetables.html">vegetables</a>
-    					,
-    					<a href="soy.html">soy products</a>
-    					,
-    					<a href="meat_seafood.html">
-    						meat, eggs, and seafood
-    					</a>
-    					,
-    					<a href="cooking.html">cooking</a>
-    					,
-    					<a href="drinks.html">drinks</a>
-    					,
-    					<a href="condiments.html">condiments</a>
-    					,
-    					<a href="grains.html">grains</a>
-    					,
-    					<a href="mushrooms.html">
-    						mushrooms and fungus
-    					</a>
-    					,&nbsp;
-    					<a href="sweets.html">dessert</a>
-    					,
-    					<a href="chinese_food.html">Chinese food</a>
-    					,
-    					<a href="western_food.html">Western food</a>
-    					, and
-    					<a href="nutrition.html">nutrition</a>
-    					.&nbsp; Article:
-    					<a href="bones_article.html">
-    						Does Fruit Strengthen Bones more than Milk?
-    					</a>
-    				</p>
-    			</td>
-    		</tr>
-    	</tbody>
-    </table>
-    <hr/>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Home</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="tools.php">Tools</a></li>
+            <li class="active"><a href="culture.php">Culture</a></li>
+            <li><a href="reference.php">Reference</a></li>
+            <li><a href="classics.php">Classics</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+    <div class="container">
+      <h2>Chinese Culture</h2>
+
+  	  <h3>Decorative Designs in Chinese Art</h3>
+  	  <p>
+        <a href='decorative_designs.php'>Decorative Designs in Chinese Art</a> &mdash;
+  	    This article describes decorative art on pottery, bronze ware, and many other art works and artifacts. 
+        Decorative art is distinguished from fine art in that decorative art is most often uses repeated designs and is 
+        applied to artifacts that have functions in themselves. Decorative art has a fascinating story to tell about 
+        Chinese culture that is not as easily told using words alone.
+        <img class="pull-right" src='images/firedesign_small.png' alt='Fire Design 火纹' title='Fire Design 火纹'/></a>
+  	  </p>
+  	  <h3>中华民族 Chinese Ethnic Groups</h3>
+  	  <p>
+        <a href='ethnic_groups.php'>Chinese Ethnic Groups</a> &mdash;
+  	    China has 56 ethnic groups.  The English and Chinese names for each and the
+  	    regions that they inhabit are listed here.
+  	  </p>
+
+      <h3>Introduction to Calligraphy</h3>
+      <p>
+        <a href='/calligraphy.php'>Introduction to Calligraphy 书法入门 </a> &mdash;
+        Introduces materials, holding and moving the brush, drawing strokes, fundamental strokes (Yanti), and
+    	copying.
+      </p>
+
+      <h3>Words of Thanks</h3>
+      <p>
+        <a href='/culture_ganenci.php'>感恩词 Words of Thanks</a>
+      </p>
+
+      <h3 class="portlet">Historic Sites and Scenic Places 名胜古迹</h3>
+      <h4>Temple of Ancient Monarchs</h4>
+      <p>
+        <a href='/history_lidaidiwangsi.php'>Temple of Ancient Monarchs 历代帝王庙</a> &mdash;
+    	The Temple of Ancient Monarchs was first built in the ninth year of the Ming Emperor 
+    	Jia Jing (1530) ...
+      </p>
+      <h4>Chinese Buddhism</h4>
+      <p>
+        <a href='/buddhism_chinese.html'>Chinese Buddhism 中国佛教</a> &mdash;
+        Lists, maps, and background for Chinese Buddhist sites
+      </p>
+      <h4>Hongluo Temple</h4>
+      <p>
+    	<a href='/history_hongluosi.php'>Hongluo Temple 红螺寺</a> &mdash;
+    	This temple was built by the senior monk Fo Tucheng in the fourth year of the reign of Emperor Xian Kang 
+    	in the Eastern Jin period (338 AD).
+        <a class="pull-right" href='illustrations_use.php?mediumResolution=huangluo3_med.jpg'><img class='use' 
+           src='images/huangluo3_med.jpg' alt='红螺寺 Hongluo Temple' 
+           title='红螺寺Hongluo Temple'/></a>
+      </p>
+      <h4>Prince Gong's Palace</h4>
+      <p>
+    	<a href='/history_gongwangfu.php'>Prince Gong's Palace 恭王府</a> &mdash;
+    	Prince Gong's Palace is one of Beijing's still existing, most refined, best preserved, and complete Qing 
+    	Dynasty declared bureau. 
+      </p>
+
+      <hr/>
       <p>
         Copyright Fo Guang Shan Nan Tien Institute 佛光山南天大學 2013-2015, 
         <a href="http://www.nantien.edu.au/" title="Fo Guang Shan Nan Tien Institute">www.nantien.edu.au</a>. 
@@ -297,6 +119,12 @@
         <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. 
         See <a href='about.php'>About</a>. Please send comments to alex@chinesenotes.com.
       </p>
-      <p>This page was last updated on February 1, 2015.</p>
+      <p>This page was last updated on February 2, 2015.</p>
+    </div> <!-- /.container -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-sanitize.js"></script>
+    <script src="script/dictionary.js"></script>
   </body>
 </html>
--- web/tools.php
+++ web/tools.php
@@ -1,156 +1,99 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
-    <title>Chinese Notes - Language Tools 汉语笔记 - 语言工具</title>
-    <link rel="shortcut icon" href="/favicon.ico"/>
-    <link rel="stylesheet" type="text/css" href="styles.css"/>
-    <link media="only screen and (max-device-width: 480px)" href="styles-small-device.css" type= "text/css" 
-          rel="stylesheet"/>
-    <meta name="keywords"
-          content="Chinese Notes - Language Tools 汉语笔记 - 语言工具"/>
-    <meta name="description"
-          content="Chinese Notes - Language Tools 汉语笔记 - 语言工具"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Chinese language notes">
+    <link rel="shortcut icon" href="images/ren.png" type="image/jpeg" />
+    <title>Chinese language notes</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="chinesenotes.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
   </head>
   <body>
-    <div class="menubar">
-      <a class='button' href='index.html'>Home<span class='cn'> - 首页</span></a>
-      <a class='selected' href='tools.php'>Tools<span class='cn'> - 工具</span></a>
-      <a class='button' href='culture.php'>Culture<span class='cn'> - 文化</span></a>
-      <a class='button' href='reference.php'>Reference<span class='cn'> - 参考</span></a>
-      <a class='button' href='classics.php'>Classics<span class='cn'> - 古文</span></a>
+    <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-59206430-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <div class="starter-template">
+      <div class="row">
+        <div class="span2"><img id="logo" src="images/ren.png" alt="Logo" class="pull-left"/></div>
+        <div class="span7"><h1>Chinese Notes</h1></div>
+      </div>
     </div>
-    <div class="breadcrumbs">
-      <a href="index.html">Chinese Notes 中文笔记</a> &gt; 
-      Language Tools 语言工具
-    </div>      
-    <h1>Language Tools 语言工具</h1>
-    <table class="portlet">
-    	<tbody>
-    		<tr>
-    			<th class="portlet" colspan="2">
-    				<h2 class="portlet">New 新</h2>
-    			</th>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h3 class="portlet">
-    					Chinese Phonetics 漢語語音學
-    				</h3>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h4>
-    					<a href="phonetics.html">Phonetics 語音學</a>
-    				</h4>
-    				<p>
-    				    Chinese Phonetics 漢語語音學
-    				</p>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h3 class="portlet">
-    					汉英词典 Chinese-English Dictionary
-    				</h3>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h4>
-    					<a href="topics_explorer.php">
-    						汉英词典 Chinese-English Dictionary
-    					</a>
-    				</h4>
-    				<p>
-    				    查词典或浏览话题。
-    					本网站汉英词典现在有所有汉语水平考试词汇包括 11 000 以上词包括语法和例子。
-    					<br />
-    					Search the dictionary or browse by topic.
-    					The chinesenotes.com Chinese-English dictionary now has all the Chinese Language
-    					Proficiency Test (HSK) vocabulary, including over 11,000 words with
-    					grammar and example sentences.
-    				</p>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h4>
-    				    <a href='mobile_app.html'>手机语言工具 Mobile Language Application</a>
-    				</h4>
-                    <p>
-                        正在开发中手机语言工具使用手机存储存语言数据。
-                    <br/>
-                      Currently under development, the Mobile Language Application makes use of an
-                      offline cache to store language data.
-    				</p>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h4>
-    					<a href='/bookmarklets.php'>
-    						Bookmarklets
-    					</a>
-    				</h4>
-                    <p>
-                        搜索中文笔记汉英词典。
-                         用谷歌搜索本网站与新华网。
-                    <br/>
-                      Bookmarklet scripts that search the chinesenotes.com dictionary.
-    				  Use Google to search this web site and also Xinhua Net.
-    				</p>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h3 class="portlet">
-    					其他语言工具与资料
-    					<br />
-    					Other Language Tools
-    				</h3>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h4>
-    					<a href='/character_search.php'>
-    						Chinese English Character Dictionary 汉英字典 
-    					</a>
-    				</h4>
-                    <p>
-                        Look up a character 查字 
-    				</p>
-    			</td>
-    		</tr>
-    		<tr>
-    			<td colspan="2">
-    				<h4>
-    					<a href='/pinyin_format.php'>
-    						Pinyin Formatting Tool 写拼音
-    					</a>
-    				</h4>
-                    <p>
-                         写汉语拼音象 ni3hao3 工具就变成 nǐhǎo。
-                    <br/>
-    					Formats pinyin written like ni3hao3 to the form nǐhǎo.
-    				</p>
-    			</td>
-    		</tr>
-    	</tbody>
-    </table>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Home</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="tools.php">Tools</a></li>
+            <li><a href="culture.php">Culture</a></li>
+            <li><a href="reference.php">Reference</a></li>
+            <li><a href="classics.php">Classics</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+    <div class="container">
+
+    <h2>Language Tools</h2>
+    <h3>Chinese Phonetics</h3>
+    <p><a href="phonetics.html">Phonetics 語音學</a></p>
+    <h3>Vocabulary Lists</h3>
+    <p><a href="topics_explorer.php">Chinese-English Dictionary</a> &mdash;
+      Search the dictionary or browse by topic.
+      The chinesenotes.com Chinese-English dictionary now has all the Chinese Language
+      Proficiency Test (HSK) vocabulary, including over 11,000 words with
+      grammar and example sentences.
+    </p>
+    <h3>Chinese English Character Dictionary 汉英字典</h3>
+    <p>
+      <a href='/character_search.php'>Chinese English Character Dictionary 汉英字典</a> &mdash;
+      Look up a character 查字 
+    </p>
+    <h4>Pinyin Formatting Tool</h4>
+    <p>
+      <a href='/pinyin_format.php'>Pinyin Formatting Tool 写拼音</a><br/>
+      写汉语拼音象 ni3hao3 工具就变成 nǐhǎo。<br/>
+      Formats pinyin written like ni3hao3 to the form nǐhǎo.
+    </p>
+    <h4>Other Resources</h4>
     <p>
     	<a href="resources.html">Resources</a>
-    	- other resources for reference and learning Mandarin
+    	&mdash; other resources for reference and learning Mandarin
     	<br />
     </p>
-    <hr/>
+
+      <hr/>
       <p>
         Copyright Fo Guang Shan Nan Tien Institute 佛光山南天大學 2013-2015, 
         <a href="http://www.nantien.edu.au/" title="Fo Guang Shan Nan Tien Institute">www.nantien.edu.au</a>. 
         This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. See <a href='about.php'>About</a>. Please send comments to alex@chinesenotes.com.
       </p>
-      <p>This page was last updated on February 1, 2015.</p>
+      <p>This page was last updated on February 2, 2015.</p>
+    </div> <!-- /.container -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-sanitize.js"></script>
+    <script src="script/dictionary.js"></script>
   </body>
 </html>
