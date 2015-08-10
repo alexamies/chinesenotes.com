34faff57785a5f0c0c465c6da810fe46f7ed9264
alexamies <alexamies@gmail.com>
2015-08-09 21:39:58 -0700

    Generation of HTML popover for classical Chinese text.

--- go/src/cnreader/cnreader.go
+++ go/src/cnreader/cnreader.go
@@ -1,13 +1,27 @@
 package main
 // Command line utility to mark up HTML files with Chinese notes.
 
-import analysis "cnreader/analysis"
+import (
+	"flag"
+	"fmt"
+	analysis "cnreader/analysis"
+)
 
+/*
+ * Entry point for the chinesenotes command line tool.
+ */
 func main() {
+	// Command line flags
+	var infile = flag.String("infile", "testdata/test.html", "Input file")
+	var outfile = flag.String("outfile", "testoutput/test-gloss.html", "Output file")
+	flag.Parse()
+	fmt.Printf("main: input file: %s, output file: %s\n", *infile, *outfile)
+
+	// Read in dictionary
 	analysis.ReadDict("../../../data/words.txt")
-	filename := "../../../web/classical_chinese-raw.html"
-	text := analysis.ReadText(filename)
+
+	// Read text and perform vocabulary analysis
+	text := analysis.ReadText(*infile)
 	tokens, vocab := analysis.ParseText(text)
-	outfile := "../../../web/classical_chinese.html"
-	analysis.WriteDoc(tokens, vocab, outfile)
+	analysis.WriteDoc(tokens, vocab, *outfile)
 }
