f8e7ab647b8e88c143d990a7081c7342d21135d6
alexamies <alexamies@gmail.com>
2015-09-05 07:20:35 -0700

    Start reorg into collection of texts

---
+++ web/articles.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
+    <title>Chinese Notes - Articles 汉语笔记 - 文章</title>
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="styles.css"/>
+    <meta name="keywords"
+          content="A web site for learning (Mandarin) Chinese. Bilingual Articles 双言文章"/>
+    <meta name="description"
+          content="A web site for learning (Mandarin) Chinese. Bilingual articles on current affairs, business, economics, sports, science, technology, and the environment. 实事，经济，商务，体育，科学，科技，环保等双言文章。"/>
+  </head>
+  <body>
+<?php
+  	require_once 'ad_header.txt' ;
+?>
+    <div class="menubar">
+      <a class='button' href='index.html'>Home - 首页</a>
+      <a class='button' href='tools.php'>Tools - 工具</a>
+      <a class='button' href='buddhism_toc.php'>佛教  Buddhism</a>
+      <a class='selected' href='culture.php'>Culture - 文化</a>
+      <a class='button' href='reference.php'>Reference - 参考</a>      
+      <a class='button' href='classics.php'>Classics - 古文</a>
+      <a class='button' href='developers.php'>Developers - 软件</a>
+    </div>
+    <div class="breadcrumbs">
+      <a href="index.html">Chinese Notes 中文笔记</a> &gt; 
+      <a href='culture.php'>Culture - 文化</a> &gt; 
+      Articles 文章
+    </div>
+    <h1>Articles 文章</h1>
+    <p>
+      从大陆媒体中英双言包括口音和生词定义。
+      <br/>
+      Chinese and English versions of the articles from the Mainland media with audio and 
+      links to all the vocabulary used.
+    </p>
+            <table class="portlet">
+              <tbody>
+                <tr>
+                  <th class="portlet" colspan="2">
+                    <h2 class="portlet">新&nbsp; New </h2>
+                    <div class="author">2008年 8月 19日</div>
+                  </th>
+                </tr>
+                <tr>
+                  <td width="75%">
+                    <h3 class="portlet">体育 Sport</h3>
+                  </td>
+                  <td width="25%" rowspan="2">
+                    <img src='images/200px-Olympic_flag.svg.png' alt='五环旗 Olympic Flag' title='五环旗 Olympic Flag'/>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                  	<p>
+                  	  <a href='sport_dongzu.php'>侗族小伙陆永:教练眼中的"特殊"孩子</a>（举重）<br/>
+                  	  <a href='/sport_dongzu.php'>In the coach's eyes the young Dong boy Lu Yong is a gifted child</a> (Weightlifting)<br/>
+                  	</p>
+                  	<p>
+                  	  <a href='article_olympic_20days.php'>奏响“和平、友谊、进步”的北京乐章——写在北京奥运会倒计时20天</a><br/>
+                  	  <a href='article_olympic_20days.php'>A Dedication of “Peace, Friendship, and Progress”: a Beijing's Symphony</a><br/>
+                  	  <a href='article_olympic_20days.php'>20 Days and Counting Down to the Beijing Olympics</a>
+                  	</p>
+                  	<p><a href='article_olympic_beijing.php'>奥运元素融入古老北京 <br />Old Beijing and the Olympics</a></p>
+    				<p>
+    					<a href="article_olympic_work.php">
+    						三年来北京奥运会筹办工作进展顺利
+    						<br />
+    						Three years on Preparation Work for the
+    						Beijing Olympic Games is Progressing
+    						Smoothly
+    					</a>
+    				</p>
+                  </td>
+                </tr>
+               </tbody>
+            </table>
+            <br/>
+    <table class="portlet">
+    	<tbody>
+    		<tr>
+    			<td width="75%">
+    				<h3 class="portlet">
+    					科学 环保
+    					<br />
+    					Science and Environment
+    				</h3>
+    			</td>
+    			<td width="25%" rowspan="2">
+    				<img src='images/half_dome_small.jpg'
+    					alt='环保优美胜地 Yosemite' />
+    			</td>
+    		</tr>
+    		<tr>
+    			<td>
+    				<p>
+    					<a href="article_spring_festival.php">
+    						欢度春节其实还有个环保问题
+    					</a>
+    					<br />
+    					<a href="article_spring_festival.php">
+    						Joyfully spending Spring Festival also has
+    						an environmental protection question
+    					</a>
+    				</p>
+    				<p>
+    					<a href="article_satellite.php">
+    						气象局：空间天气利于“嫦娥一号”10月下旬发射
+    					</a>
+    					<br />
+    					<a href="article_satellite.php">
+    						Meteorological Administration: Space and
+    						weather favorable for “Chang E No. 1” launch
+    						in late October
+    					</a>
+    				</p>
+    				<p>
+    					<a href="cnnic.php">
+    						中国互联网
+    					</a>
+    					<br />
+    					<a href="cnnic.php">
+    						Chinese Internet
+    					</a>
+    				</p>
+    			</td>
+    		</tr>
+    	</tbody>
+    </table>
+    <br />
+    <table class="portlet">
+    	<tbody>
+    		<tr>
+    			<td width="75%">
+    				<h3 class="portlet">
+    					经济商务瞩目
+    					<br />
+    					Business and Economics Focus
+    				</h3>
+    			</td>
+    			<td width="25%" rowspan='2'>
+    				<img src='images/yang_guang_window_med.jpg'
+    					alt='公寓 Apartment' />
+    			</td>
+    		</tr>
+    		<tr>
+    			<td>
+    				<p>
+    					<a href="article_housing.php">地价大跌了，房价会怎样？</a>
+    					<br />
+    					<a href="article_housing.php">
+    						If the price of land drops, what about
+    						housing prices?
+    					</a>
+    				</p>
+    				<p>
+    					<a href="article_public_companies.php">
+    						国资委正式否认“30家中央企业将整体上市”传闻
+    					</a>
+    					<br />
+    					<a href="article_public_companies.php">
+    						State-Owned Assets Supervision and
+    						Administration completely denies the romour
+    						that “30 Central Enterprises will be
+    						Completely Floated on the Stock Exchange”
+    					</a>
+    				</p>
+    				<p>
+    					<a href="article_personal_finance.php">
+    						进入加息通道后如何关注储蓄理财
+    						<br />
+    						How to Monitor Savings and Personal Finance
+    						After Interest Rate Rises
+    					</a>
+    				</p>
+    				<p>
+    					<a href="strong_economy.php">中国在改革开放</a>
+    					<br />
+    					<a href="strong_economy.php">
+    						China Reforms and Opens
+    					</a>
+    				</p>
+    			</td>
+    		</tr>
+    	</tbody>
+    </table>
+    <br />
+    <table class="portlet">
+    	<tbody>
+    		<tr>
+    			<td width="75%">
+    				<h3 class="portlet">
+    					时事经纬 Current Events
+    				</h3>
+    			</td>
+    			<td width="25%" rowspan='2'>
+    				<img src='images/morning_market_med.jpg'
+    					alt='市场 Market' />
+    			</td>
+    		</tr>
+    		<tr>
+    			<td>
+    				<p>
+    					<a href="article_one_child.php">
+    						广西博白县大批干部因违反计生国策被处罚
+    					</a>
+    					<br />
+    					<a href="article_one_child.php">
+    						In Bobai County, Guanxi a large number of
+    						officials have been punished for violation
+    						of the One Child Policy
+    					</a>
+    				</p>
+    				<p>
+    					<a href="article_orphan.php">
+    						海南文昌爱心学校孤儿们的幸福生活
+    					</a>
+    					<br />
+    					<a href="article_orphan.php">
+    						The happy life of orphans at Hainan's School
+    						of Compassion
+    					</a>
+    				</p>
+    				<p>
+    					<a href="article_burma.php">美国对缅甸以压促变图谋昭然若揭</a>
+    					<br />
+    					<a href="article_burma.php">
+    						US uses pressure and rushes plotting for
+    						rebellion and conspiracy in Myanmar in a
+    						supposed illustrious way
+    					</a>
+    				</p>
+    				<p>
+    					<a href="red_mosque.php">红色清真寺</a>
+    					<br />
+    					<a href="red_mosque.php">The Red Mosque</a>
+    				</p>
+    			</td>
+    		</tr>
+    	</tbody>
+    </table>
+<?php
+  	require_once 'footer2.txt';
+?>
+  </body>
+</html>
