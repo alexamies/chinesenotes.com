1632a6ab25e8b29a087c00a141bc5c50eed9af92
alexamies <alexamies@gmail.com>
2016-04-17 20:27:27 -0700

    Fixed mistake in processing boilerplate

--- go/src/cnreader/analysis/analysis.go
+++ go/src/cnreader/analysis/analysis.go
@@ -252,7 +252,7 @@ func ParseText(text string, colTitle string, document *corpus.CorpusEntry) (
 				//log.Printf("analysis.ParseText: i = %d, j = %d, w = %s\n", i, j, w)
 				if wsArray, ok := wdict[w]; ok {
 					//log.Printf("analysis.ParseText: found word %s, i = %d\n", w, i)
-					if wsArray[0].Notes != "CBETA boilerplate" {
+					if wsArray[0].Notes == "CBETA boilerplate" {
 						//log.Printf("analysis.ParseText: boilerplate\n")
 						tokens.PushBack(chunk)
 						lastHWPtr = new(dictionary.HeadwordDef)
