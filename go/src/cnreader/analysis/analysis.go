be0ddac9338a8ddd792194a096b6d635066771e4
alexamies <alexamies@gmail.com>
2016-10-08 21:52:11 -0700

    Reduced the max number of collocations and useage context further

--- go/src/cnreader/analysis/analysis.go
+++ go/src/cnreader/analysis/analysis.go
@@ -35,7 +35,7 @@ const MAX_UNKOWN_OUTPUT = 50
 const UNIGRAM_FILE = "unigram.txt"
 
 // Max usage elements for a word
-const MAX_USAGE = 25
+const MAX_USAGE = 20
 
 // Max number of occurrences of same title in a list of word usages
 const MAX_TITLE = 5
