b5fc6f0ad3e36699b9f4a89daf28563ebfd0c363
alexamies <alexamies@gmail.com>
2018-06-02 12:58:19 -0700

    History of the Southern Dynasties

--- bin/cnreader.sh
+++ bin/cnreader.sh
@@ -30,6 +30,7 @@ mkdir $WEB_DIR/analysis/lunyu
 mkdir $WEB_DIR/analysis/mengzi
 mkdir $WEB_DIR/analysis/mozi
 mkdir $WEB_DIR/analysis/nanqishu
+mkdir $WEB_DIR/analysis/nanshi
 mkdir $WEB_DIR/analysis/sanguozhi
 mkdir $WEB_DIR/analysis/shanhaijing
 mkdir $WEB_DIR/analysis/shangshu
@@ -79,6 +80,7 @@ mkdir $WEB_DIR/mengzi
 mkdir $WEB_DIR/mozi
 mkdir $WEB_DIR/mp3
 mkdir $WEB_DIR/nanqishu
+mkdir $WEB_DIR/nanshi
 mkdir $WEB_DIR/sanguozhi
 mkdir $WEB_DIR/script
 mkdir $WEB_DIR/shanhaijing
