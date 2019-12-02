3c27764c1bbdcafae221b2af46783b1b425b4f15
alexamies <alexamies@gmail.com>
2019-12-01 17:41:55 -0800

    Processing of static files for e2e test

---
+++ e2etest/e2ecopy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copy and tar static files for end to end test
+mkdir static
+cp ../web-staging/index.html static/.
+cp ../web-staging/idioms.html static/.
+cp ../web-staging/texts.html static/.
+cp ../web-staging/hongloumeng.html static/.
+mkdir static/hongloumeng
+cp ../web-staging/hongloumeng/hongloumeng001.html static/.
+mkdir static/dist
+cp ../web-staging/dist/*.css static/dist/.
+cp ../web-staging/dist/*.js static/dist/.
+cp ../web-staging/dist/*.json static/dist/.
+mkdir static/images
+cp ../web-staging/images/*.png static/images/.
+tar -czf static.tar.gz static/
