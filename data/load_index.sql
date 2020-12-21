e98c0bca603143e1531295d028df5749e2f31601
jifusfo <jifusfo@outlook.com>
2020-12-20 20:56:06 -0800

    Words

--- data/load_index.sql
+++ data/load_index.sql
@@ -5,3 +5,5 @@ delete from document;
 
 LOAD DATA LOCAL INFILE 'data/corpus/collections.csv' INTO TABLE collection CHARACTER SET utf8mb4 LINES TERMINATED BY '\n' IGNORE 2 LINES;
 LOAD DATA LOCAL INFILE 'data/corpus/documents.csv' INTO TABLE document CHARACTER SET utf8mb4 LINES TERMINATED BY '\n';
+
+select count(*) from document;
